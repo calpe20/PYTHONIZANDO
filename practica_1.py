370db5b2f659cf9a3e6b6e6c8cbf15b302e823ba
calpe20 <calpe20@gmail.com>
2012-06-07 18:07:52 -0500

    agregar
    agregando practica 01

---
+++ practica_1.py
@@ -0,0 +1,10 @@
+#!usr/bin/dev/ python
+#--*-- conding: utf-8-*-
+# ESTRUCTURA CONDICIONA IF
+import random
+''' hola a todos '''
+y = 5
+x = random.randint(1,10)
+print x
+if x == y:
+    print str(y) + ' es igual al numero generado'
